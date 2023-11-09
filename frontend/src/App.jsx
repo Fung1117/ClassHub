3f56947499f5cb77bd918d8e252bb155b97ea342
Pun King Fung <99673664+Fung1117@users.noreply.github.com>
2023-11-09 22:46:17 +0800

    update the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,10 +23,22 @@ const { Header, Sider, Content, Footer } = Layout;
 
 const App = () => {
   const [collapsed, setCollapsed] = useState(false);
-  const [selectedKeys, setSelectedKeys] = useState(['1']);
+  const [selectedKeys, setSelectedKeys] = useState(['0']);
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+
+  useEffect(() => {
+    const storedKey = localStorage.getItem('selectedKey');
+    if (storedKey) {
+      setSelectedKeys([storedKey]);
+    }
+  }, []);
+
+  const handleMenuItemClick = (key) => {
+    setSelectedKeys(key);
+    localStorage.setItem('selectedKey', key);
+  };
 
   const menuItems = [
     { key: '1', icon: <HomeOutlined />, label: 'DashBoard', link: '/' },
@@ -38,7 +50,7 @@ const App = () => {
   return (
     <Layout>
         <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-          <Link to="/" onClick={() => setSelectedKeys(['1'])}>
+          <Link to="/" onClick={() => handleMenuItemClick(['1'])}>
             <div style={{ textAlign: 'center', padding: '16px' }}>
               <GiEvilBook style={{ color: 'white', fontSize: 36 }} />
             </div>
@@ -46,11 +58,11 @@ const App = () => {
           <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} selectedKeys={selectedKeys} >
             {menuItems.map((item) => (
               <Menu.Item key={item.key} icon={item.icon}>
-                <Link to={item.link} onClick={() => setSelectedKeys([item.key])}>{item.label}</Link>
+                <Link to={item.link} onClick={() => handleMenuItemClick([item.key])}>{item.label}</Link>
               </Menu.Item>
             ))}
             <Menu.Item key="5" icon={<LogoutOutlined />} style={{ position: 'absolute', bottom: 10, left: 0}}>
-              <Link to="/logout" onClick={() => setSelectedKeys(['5'])}>Logout</Link>
+              <Link to="/logout" onClick={() => handleMenuItemClick(['5'])}>Logout</Link>
             </Menu.Item>
           </Menu>
         </Sider>
@@ -58,7 +70,7 @@ const App = () => {
         <Header style={{ padding: 0, background: colorBgContainer, height: '100px' }}>
           <Space>
             <Image src={HeaderLogo} preview={false} alt="Logo" style={{ height: '100px', marginLeft: '16px', verticalAlign: 'top' }} />
-            <Link to="/login" onClick={() => setSelectedKeys(['0'])}>
+            <Link to="/login" onClick={() => handleMenuItemClick(['0'])}>
               <Button type="primary" size="large" icon={<LoginOutlined />} style={{ position: 'absolute', top: 30, right: 30 }}>
                 Login
               </Button>
--- frontend/src/pages/Logout.jsx
+++ frontend/src/pages/Logout.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Image, Result } from 'antd';
 import logoutImage from '../assets/logout-image.svg';
+import axios from 'axios';
 
 const Logout = () => {
+
     return (
         <div>
             <Image src={logoutImage} preview={false}/>

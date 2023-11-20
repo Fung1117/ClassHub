6befe38118d7095bbf1d0e118a259bc89408d6d7
Pun King Fung <99673664+Fung1117@users.noreply.github.com>
2023-11-20 20:54:57 +0800

    update the photo of homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Flex, Card } from 'antd';
 import { RightOutlined } from '@ant-design/icons';
-import Wellcome from '../assets/wellcomeImage.svg'
+import Wellcome from '../assets/wellcomeImage.svg';
+import SoonClass from '../assets/SoonClass.svg';
+import TimeTable from '../assets/timeTable.jpg';
 
 const { Meta } = Card;
 
@@ -13,12 +15,12 @@ const Home = () => {
                     <Meta title={`Welcome to ICMS! name`} />
                 </Card>
                 <Flex justify="center" align="center" vertical gap={20} style={{ height: 650, width: '30%' }}>
-                    <Card hoverable cover={<img src={Wellcome} height={200} />} style={{ height: 320, width: '100%' }} actions={[<RightOutlined key="setting" />]}>
+                    <Card hoverable cover={<img src={SoonClass} height={200} />} style={{ height: 320, width: '100%' }} actions={[<RightOutlined key="setting" />]}>
                         <Meta
                             title="If you have a class soon, find all the details—classroom, teacher's message, Zoom link, and materials—all in one place."
                         />
                     </Card>
-                    <Card hoverable cover={<img src={Wellcome} height={200} />} style={{ height: 320, width: '100%' }} actions={[<RightOutlined key="setting" />]}>
+                    <Card hoverable cover={<div style={{ display: 'flex', justifyContent: 'center' }}> <img src={TimeTable} style={{width: 200, height: 200, objectFit: 'cover'}}/></div> } style={{ height: 320, width: '100%' }} actions={[<RightOutlined key="setting" />]}>
                         <Meta
                             title="No class now? Check your personal timetable anytime."
                         />

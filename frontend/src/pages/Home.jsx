efb556fa96cf2a71485815fd3348cd78b9179497
Pun King Fung <99673664+Fung1117@users.noreply.github.com>
2023-11-20 21:00:24 +0800

    update home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Flex, Card } from 'antd';
 import { RightOutlined } from '@ant-design/icons';
 import Wellcome from '../assets/wellcomeImage.svg';
@@ -8,6 +9,9 @@ import TimeTable from '../assets/timeTable.jpg';
 const { Meta } = Card;
 
 const Home = () => {
+
+    const navigate = useNavigate();
+
     return (
         <Flex justify="center" align="center" vertical style={{ width: '100%' }}>
             <Flex gap="middle" align="center" justify="center" style={{ width: '100%', }}>
@@ -15,12 +19,12 @@ const Home = () => {
                     <Meta title={`Welcome to ICMS! name`} />
                 </Card>
                 <Flex justify="center" align="center" vertical gap={20} style={{ height: 650, width: '30%' }}>
-                    <Card hoverable cover={<img src={SoonClass} height={200} />} style={{ height: 320, width: '100%' }} actions={[<RightOutlined key="setting" />]}>
+                    <Card hoverable cover={<img src={SoonClass} height={200} />} style={{ height: 320, width: '100%' }} actions={[<RightOutlined />]}>
                         <Meta
                             title="If you have a class soon, find all the details—classroom, teacher's message, Zoom link, and materials—all in one place."
                         />
                     </Card>
-                    <Card hoverable cover={<div style={{ display: 'flex', justifyContent: 'center' }}> <img src={TimeTable} style={{width: 200, height: 200, objectFit: 'cover'}}/></div> } style={{ height: 320, width: '100%' }} actions={[<RightOutlined key="setting" />]}>
+                    <Card hoverable cover={<div style={{ display: 'flex', justifyContent: 'center' }}> <img src={TimeTable} style={{ width: 200, height: 200, objectFit: 'cover' }} /></div>} style={{ height: 320, width: '100%' }} actions={[<RightOutlined />]}>
                         <Meta
                             title="No class now? Check your personal timetable anytime."
                         />
